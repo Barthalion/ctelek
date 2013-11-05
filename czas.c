0acb998b4adf043dda9df0438a550f0d343fa7f4
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2013-11-05 19:05:46 +0100

    Rewrite czas.c.
    - use more spaces for better readability
    - remove useless comments
    - declare iteration variables inside loops
    - get rid of conio.h; instead of using getch, add posibility to exit
      the program with 0

--- czas.c
+++ czas.c
@@ -1,15 +1,13 @@
-//program p2	KTB Oct 7,1996
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 #include <time.h>
 #include <limits.h>
 
-#define randomize() srand(unsigned(time(NULL)))
+#define randomize() srand((unsigned)time(NULL))
 #define random(a) rand()%a
 #define nmax 101
 
-typedef int t[nmax]; //a
+typedef int t[nmax];
 
 void czyt(int *n, int *z);
 void czyt1(int *n, int *z, int *rep);
@@ -23,101 +21,94 @@ void druk2(int n, t a);
 clock_t pomiar(void);
 void kopiuj_tablice(int n, t b, t a);
 
-int main()
+int main(void)
 {
-    int i,j,n,z,rep,wariant;
-    t a,b;
+    int n, z, rep, wariant;
+    t a, b;
     clock_t czas1, czas2;
-
-
     randomize();
+
+    printf("Info INT_MAX: %d\n", INT_MAX);
+
     do {
-        printf("Info INT_MAX: %d\n",INT_MAX);
-        printf("Wariant: (1/2)");
+        printf("Wariant: (0/1/2) ");
         scanf("%d",&wariant);
-        switch(wariant) {
-        case 1: { //sort i sort1
-            czyt(&n,&z);
-            generacja(n,z,a);
-            kopiuj_tablice(n,b,a);
-            druk1(n,z,a);
-            sort(n,a);
-            druk2(n,a);
-            sort1(n,b);
-            druk2(n,b);
-            break;
-        } // 1
-        case 2: { //czas
-            czyt1(&n,&z,&rep);
-            czas1=pomiar();
-            //for(j=1;j<=100;j++)
-            for(i=1; i<=rep; i++) {
-                generacja(n,z,a);
-                sort(n,a);
-            }
-            czas2=pomiar();
 
-            /* printf("clock: %d\n", czas2 - czas1); */
+        switch (wariant) {
+            case 0:
+                exit(EXIT_SUCCESS);
+            case 1: //sort i sort1
+                czyt(&n, &z);
+                generacja(n, z, a);
+                kopiuj_tablice(n, b, a);
+                druk1(n, z, a);
+                sort(n, a);
+                druk2(n, a);
+                sort1(n, b);
+                druk2(n, b);
+                break;
+            case 2: //czas
+                czyt1(&n, &z, &rep);
+                czas1 = pomiar();
+                for (int i = 1; i <= rep; i++) {
+                    generacja(n, z, a);
+                    sort(n, a);
+                }
+                czas2=pomiar();
 
-            printf("czas: %f\n", (float)(czas2 - czas1) / (float)CLK_TCK);
-            break;
-        } //2
-        default: {
-            printf("Taki wariant nie istnieje!\n");
+                printf("czas: %lf\n", (double) (czas2 - czas1) / (double) CLOCKS_PER_SEC);
+                break;
+            default:
+                printf("Taki wariant nie istnieje!\n");
         }
-        } // switch
-        printf("koniec? 0/1\n");
-    } while (getch()!='1');
-    return 0;
-} //main
+    } while (1);
+}
 
 
 void czyt(int *n, int *z)
 {
     do {
-        printf("n,z= ");
-        scanf("%d%d",n,z);
+        printf("n,z = ");
+        scanf("%d,%d", n, z);
     } while (*n > nmax);
 
-} //czyt
+}
 
 void czyt1(int *n, int *z, int *rep)
 {
     do {
-        printf("n,z,rep= ");
-        scanf("%d%d%d",n,z,rep);
+        printf("n,z,rep = ");
+        scanf("%d,%d,%d", n, z, rep);
     } while ((*n > nmax) && (*z > 32000) && (*rep > 100000));
-} //czyt1
+}
 
 void generacja(int n, int z, t a)
 {
-    int i;
-    for(i=1; i<=n; i++)
-        a[i]=random(z)+1;
-} //generacja
+    for (int i = 1; i <= n; i++)
+        a[i] = random(z)+1;
+}
 
 void druk(int n, t a)
 {
-    int i;
-    for(i=1; i<=n; i++) {
+    for (int i = 1; i <= n; i++) {
         printf("%6d", a[i]);
-        if(i % 10==0)
+        if (i % 10 == 0)
             printf("\n");
     }
-} //druk
+}
 
 void sort(int n, t a)
 {
-    int i,j,k, naj;
-    for(i=1; i<=n-1; i++) {
-        naj=a[i];
-        k=i;
-        for(j=i+1; j<=n; j++)
-            if (a[j]>naj) {
-                naj=a[j];
-                k=j;
-            }	//popraw
-        if (i!=k) {
+    int k, naj;
+    for (int i = 1; i <= n - 1; i++) {
+        naj = a[i];
+        k = i;
+        for (int j = i+1; j <= n; j++)
+            if (a[j] > naj) {
+                naj = a[j];
+                k = j;
+            } //popraw
+        if (i != k) {
             //zamien
             a[k]=a[i];
             a[i]=naj;
@@ -125,61 +116,58 @@ void sort(int n, t a)
         /*printf("\n");
         printf("i= %3d",i);
         druk(n,a); */
-    } // i
-} //sort
+    }
+}
 
-void maxindeks(int n,int first, t a, int *k)
+void maxindeks(int n, int first, t a, int *k)
 //k jest indeksem najwiekszego elementu ciagu o indeksach od first do n
 {
-    int j,x;
-    x=a[first];
-    *k=first;
-    for(j=first+1; j<=n; j++)
-        if (a[j]>x) {
-            x=a[j];
-            *k=j;
-        }
-} //maxindeks
+    int x = a[first];
+    *k = first;
 
-void sort1(int n,t a)
-{
-    int i,k,naj;
-    for(i=1; i<=n-1; i++) {
-        maxindeks(n,i,a,&k);
-        if (i!=k) {
-            naj=a[k];
-            a[k]=a[i];
-            a[i]=naj;
+    for (int i = first + 1; i <= n; i++)
+        if (a[i] > x) {
+            x = a[i];
+            *k = i;
         }
-    } // i
-} // sort1
+}
+
+void sort1(int n, t a)
+{
+    int k, naj;
+    for (int i = 1; i<= n - 1; i++) {
+        maxindeks(n, i, a, &k);
+        if (i != k) {
+            naj = a[k];
+            a[k] = a[i];
+            a[i] = naj;
+        }
+    }
+}
 
 void druk1(int n, int z, t a)
 {
     printf("\n");
-    printf("Sortowanie tablicy, n= %3d,z= %5d\n",n,z);
+    printf("Sortowanie tablicy, n = %3d, z = %5d\n", n, z);
     printf("Elementy: \n");
-    druk(n,a);
-} //druk1
+    druk(n, a);
+}
 
 void druk2(int n, t a)
 {
     printf("Posortowane: \n");
-    druk(n,a);
+    druk(n, a);
     printf("\n");
-} //druk2
+}
 
 clock_t pomiar(void)
 {
-    clock_t czas;
-    czas = clock();
-    return czas;
-}//pomiar1
+    return (clock_t) clock();
+}
 
 void kopiuj_tablice(int n, t b, t a)
 {
-    int i;
-    for(i=1; i<=n; i++)
-        b[i]=a[i];
-} //kopiuj_tablice
+    for (int i = 1; i <= n; i++)
+        b[i] = a[i];
+}
 
