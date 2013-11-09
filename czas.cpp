462d0f9ee384aa8b8d21997fb09a83e351334b86
Reikion <reikion@server.fake>
2013-11-09 20:37:30 +0100

    Rewrite czas.cpp and przyklad.cpp by Rivi

--- czas.cpp
+++ czas.cpp
@@ -1,185 +1,165 @@
-//program p2	KTB Oct 7,1996
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-#include <time.h>
-#include <limits.h>
+//program p2 KTB Oct 7,1996
 
-#define randomize() srand(unsigned(time(NULL)))
-#define random(a) rand()%a
-#define nmax 101
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
-typedef int t[nmax]; //a
+using namespace std;
 
-void czyt(int *n, int *z);
-void czyt1(int *n, int *z, int *rep);
-void generacja(int n, int z, t a);
-void druk(int n, t a);
-void sort(int n, t a);
-void maxindeks(int n,int integer, t a, int *k);
-void sort1(int n,t a);
-void druk1(int n, int z, t a);
-void druk2(int n, t a);
-clock_t pomiar(void);
-void kopiuj_tablice(int n, t b, t a);
+const int nmax = 100;
+
+inline int random(int poczatek_zakresu, int koniec_zakresu)
+{
+	return ((rand() % (koniec_zakresu + 1 + poczatek_zakresu)) - poczatek_zakresu);
+}
+
+void czytaj(int &liczba_elementow, int &zakres_wartosci, int &liczba_powtorzen);
+void generacja(int wygenerowane[], int liczba_elementow, int zakres_wartosci);
+void kopiuj_tablice(int wygenerowane[], int kopia_wygenerowanych[], int liczba_elementow);
+void drukuj(int wygenerowane[], int liczba_elementow, int zakres_wartosci = 0);
+void sortowanie(int wygenerowane[], int liczba_elementow);
+int indeks_najwiekszej_wartosci(int wygenerowane[], int liczba_elementow, int indeks_poczatkowy);
+void sortowanie2(int wygenerowane[], int liczba_elementow);
+inline clock_t pomiar();
 
 int main()
 {
-    int i,j,n,z,rep,wariant;
-    t a,b;
-    clock_t czas1, czas2;
+	bool wyjdz = 0;
+	short wybor;
+	int liczba_elementow, zakres_wartosci, liczba_powtorzen;
+	int wygenerowane[nmax], kopia_wygenerowanych[nmax];
+	clock_t czas_start, czas_koniec;
 
+	srand(unsigned(time(NULL)));
 
-    randomize();
-    do {
-        printf("Info INT_MAX: %d\n",INT_MAX);
-        printf("Wariant: (1/2)");
-        scanf("%d",&wariant);
-        switch(wariant) {
-        case 1: { //sort i sort1
-            czyt(&n,&z);
-            generacja(n,z,a);
-            kopiuj_tablice(n,b,a);
-            druk1(n,z,a);
-            sort(n,a);
-            druk2(n,a);
-            sort1(n,b);
-            druk2(n,b);
-            break;
-        } // 1
-        case 2: { //czas
-            czyt1(&n,&z,&rep);
-            czas1=pomiar();
-            //for(j=1;j<=100;j++)
-            for(i=1; i<=rep; i++) {
-                generacja(n,z,a);
-                sort(n,a);
-            }
-            czas2=pomiar();
+	while (!wyjdz) {
+		cout << "Wybierz wariant (1,2,0 - wyjscie): ";
+		cin >> wybor;
+		switch (wybor) {
+		case 0:
+			wyjdz = 1;
+			break;
+		case 1: //sortowania
+			liczba_powtorzen = 0;
+			czytaj(liczba_elementow, zakres_wartosci, liczba_powtorzen);
+			generacja(wygenerowane, liczba_elementow, zakres_wartosci);
+			kopiuj_tablice(wygenerowane, kopia_wygenerowanych, liczba_elementow);
+			drukuj(wygenerowane, liczba_elementow, zakres_wartosci);
+			sortowanie(wygenerowane, liczba_elementow);
+			drukuj(wygenerowane, liczba_elementow);
+			sortowanie2(kopia_wygenerowanych, liczba_elementow);
+			drukuj(kopia_wygenerowanych, liczba_elementow);
+			break;
+		case 2: //pomiar czasu sortowań
+			czytaj(liczba_elementow, zakres_wartosci, liczba_powtorzen);
+			czas_start = pomiar();
+			for (int i = 0; i < liczba_powtorzen; i++) {
+				generacja(wygenerowane, liczba_elementow, zakres_wartosci);
+				sortowanie(wygenerowane, liczba_elementow);
+			}
+			czas_koniec = pomiar();
 
-            /* printf("clock: %d\n", czas2 - czas1); */
+			cout << "Zegar: " << (czas_koniec - czas_start) << endl;
+			cout << "Czas: " << ((float)(czas_koniec - czas_start) / (float)CLK_TCK) << "s" << endl;
+			break;
+		default:
+			cout << "Taki wariant nie istnieje" << endl;
+		}
+	}
 
-            printf("czas: %f\n", (float)(czas2 - czas1) / (float)CLK_TCK);
-            break;
-        } //2
-        default: {
-            printf("Taki wariant nie istnieje!\n");
-        }
-        } // switch
-        printf("koniec? 0/1\n");
-    } while (getch()!='1');
-    return 0;
-} //main
+	return 0;
+}
 
-
-void czyt(int *n, int *z)
+void czytaj(int &liczba_elementow, int &zakres_wartosci, int &liczba_powtorzen)
 {
-    do {
-        printf("n,z= ");
-        scanf("%d%d",n,z);
-    } while (*n > nmax);
+	do {
+		if (liczba_powtorzen == 0) {
+			cout << "[Wprowadz liczebnosc zbioru oraz zakres wartosci oddzielone spacja] n,z = ";
+			cin >> liczba_elementow >> zakres_wartosci;
+		} else {
+			cout << "[Wprowadz liczebnosc zbioru, zakres wartosci oraz ilosc powtorzen oddzielone spacja] n,z,rep = ";
+			cin >> liczba_elementow >> zakres_wartosci >> liczba_powtorzen;
+		}
+	} while (liczba_elementow > nmax);
+}
 
-} //czyt
-
-void czyt1(int *n, int *z, int *rep)
+void generacja(int wygenerowane[], int liczba_elementow, int zakres_wartosci)
 {
-    do {
-        printf("n,z,rep= ");
-        scanf("%d%d%d",n,z,rep);
-    } while ((*n > nmax) && (*z > 32000) && (*rep > 100000));
-} //czyt1
+	int i;
+	for (i = 0; i < liczba_elementow; i++) {
+		wygenerowane[i] = random(0, zakres_wartosci);
+	}
+}
 
-void generacja(int n, int z, t a)
+void kopiuj_tablice(int wygenerowane[], int kopia_wygenerowanych[], int liczba_elementow)
 {
-    int i;
-    for(i=1; i<=n; i++)
-        a[i]=random(z)+1;
-} //generacja
+	for (int i = 0; i < liczba_elementow; i++) {
+		kopia_wygenerowanych[i] = wygenerowane[i];
+	}
+}
 
-void druk(int n, t a)
+void drukuj(int wygenerowane[], int liczba_elementow, int zakres_wartosci)
 {
-    int i;
-    for(i=1; i<=n; i++) {
-        printf("%6d", a[i]);
-        if(i % 10==0)
-            printf("\n");
-    }
-} //druk
+	if (zakres_wartosci == 0) {
+		cout << "Posortowane: " << endl;
+	} else {
+		cout << endl << "Sortowanie tablicy, n = " << liczba_elementow << ", z = " << zakres_wartosci << endl << endl << "Wygenerowane elementy: " << endl;
+	}
 
-void sort(int n, t a)
+	for (int i = 0; i < liczba_elementow; i++) {
+		cout << wygenerowane[i] << " ";
+		if ((i % 10 == 0) && (i != 0)) {
+			cout << endl;
+		}
+	}
+	cout << endl << endl;
+}
+
+void sortowanie(int wygenerowane[], int liczba_elementow)
 {
-    int i,j,k, naj;
-    for(i=1; i<=n-1; i++) {
-        naj=a[i];
-        k=i;
-        for(j=i+1; j<=n; j++)
-            if (a[j]>naj) {
-                naj=a[j];
-                k=j;
-            }	//popraw
-        if (i!=k) {
-            //zamien
-            a[k]=a[i];
-            a[i]=naj;
-        }
-        /*printf("\n");
-        printf("i= %3d",i);
-        druk(n,a); */
-    } // i
-} //sort
+	int indeks_maksymalnej, wartosc_maksymalna;
+	for (int i = 0; i < liczba_elementow - 1; i++) {
+		wartosc_maksymalna = wygenerowane[i];
+		indeks_maksymalnej = i;
+		for (int j = i; j < liczba_elementow; j++)
+			if (wygenerowane[j] > wartosc_maksymalna) {
+				wartosc_maksymalna = wygenerowane[j];
+				indeks_maksymalnej = j;
+			}
+		if (i != indeks_maksymalnej) {
+			wygenerowane[indeks_maksymalnej] = wygenerowane[i];
+			wygenerowane[i] = wartosc_maksymalna;
+		}
+	}
+}
 
-void maxindeks(int n,int first, t a, int *k)
-//k jest indeksem najwiekszego elementu ciagu o indeksach od first do n
+int indeks_najwiekszej_wartosci(int wygenerowane[], int liczba_elementow, int indeks_poczatkowy)
 {
-    int j,x;
-    x=a[first];
-    *k=first;
-    for(j=first+1; j<=n; j++)
-        if (a[j]>x) {
-            x=a[j];
-            *k=j;
-        }
-} //maxindeks
+	int wartosc_maksymalna, indeks_maksymalnej;
+	wartosc_maksymalna = wygenerowane[indeks_poczatkowy];
+	indeks_maksymalnej = indeks_poczatkowy;
+	for (int i = indeks_poczatkowy; i < liczba_elementow; i++)
+		if (wygenerowane[i] > wartosc_maksymalna) {
+			wartosc_maksymalna = wygenerowane[i];
+			indeks_maksymalnej = i;
+		}
+	return indeks_maksymalnej;
+}
 
-void sort1(int n,t a)
+void sortowanie2(int wygenerowane[], int liczba_elementow)
 {
-    int i,k,naj;
-    for(i=1; i<=n-1; i++) {
-        maxindeks(n,i,a,&k);
-        if (i!=k) {
-            naj=a[k];
-            a[k]=a[i];
-            a[i]=naj;
-        }
-    } // i
-} // sort1
+	int indeks_maksymalnej, najwiekszy_element;
+	for (int i = 0; i < liczba_elementow; i++) {
+		indeks_maksymalnej = indeks_najwiekszej_wartosci(wygenerowane, liczba_elementow, i);
+		if (i != indeks_maksymalnej) {
+			najwiekszy_element = wygenerowane[indeks_maksymalnej];
+			wygenerowane[indeks_maksymalnej] = wygenerowane[i];
+			wygenerowane[i] = najwiekszy_element;
+		}
+	}
+}
 
-void druk1(int n, int z, t a)
+inline clock_t pomiar()
 {
-    printf("\n");
-    printf("Sortowanie tablicy, n= %3d,z= %5d\n",n,z);
-    printf("Elementy: \n");
-    druk(n,a);
-} //druk1
-
-void druk2(int n, t a)
-{
-    printf("Posortowane: \n");
-    druk(n,a);
-    printf("\n");
-} //druk2
-
-clock_t pomiar(void)
-{
-    clock_t czas;
-    czas = clock();
-    return czas;
-}//pomiar1
-
-void kopiuj_tablice(int n, t b, t a)
-{
-    int i;
-    for(i=1; i<=n; i++)
-        b[i]=a[i];
-} //kopiuj_tablice
-
+	return clock();
+}
